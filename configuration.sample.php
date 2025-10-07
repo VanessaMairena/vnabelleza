86c19d317a24721aa0e60cfecec4f917b519e68d
Vanessa <trejosvanessa91@gmail.com>
2025-10-07 13:58:19 -0600

    feat: instalación base de Joomla y configuración inicial

---
+++ configuration.sample.php
@@ -0,0 +1,16 @@
+<?php
+class JConfig {
+    public $offline = '0';
+    public $offline_message = 'Sitio temporalmente en mantenimiento.';
+    public $sitename = 'VNABelleza';
+    public $editor = 'tinymce';
+    public $debug = '0';
+    public $dbtype = 'mysqli';
+    public $host = 'localhost';
+    public $user = 'root';
+    public $password = '';
+    public $db = 'vnabelleza_db';
+    public $dbprefix = 'vna25_';
+    public $live_site = '';
+    public $secret = 'changeme';
+}
